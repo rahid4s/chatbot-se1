9d7934c2454c59c6260653a34e06e6b10b7ccfa6
rahid4s <rahid4a@gmal.com>
2023-12-04 21:54:26 -0600

    init

---
+++ lib/app/app.dart
@@ -0,0 +1,30 @@
+import 'package:chatbot/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:chatbot/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:chatbot/ui/views/home/home_view.dart';
+import 'package:chatbot/ui/views/startup/startup_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+// @stacked-import
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: HomeView),
+    MaterialRoute(page: StartupView),
+    // @stacked-route
+  ],
+  dependencies: [
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: RouterService),
+    // @stacked-service
+  ],
+  bottomsheets: [
+    StackedBottomsheet(classType: NoticeSheet),
+    // @stacked-bottom-sheet
+  ],
+  dialogs: [
+    StackedDialog(classType: InfoAlertDialog),
+    // @stacked-dialog
+  ],
+)
+class App {}
